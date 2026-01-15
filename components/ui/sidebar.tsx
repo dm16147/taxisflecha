4c7c47bd0f26148830fd0ef8bffbb0e16796bf6c
dm16147 <domingosmachado2001@gmail.com>
2026-01-15 13:38:12 +0000

    #14 fix build

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -410,7 +410,7 @@ function SidebarGroupLabel({
         "group-data-[collapsible=icon]:-mt-8 group-data-[collapsible=icon]:opacity-0",
         className
       )}
-      {...props}
+      {...props as any}
     />
   )
 }
@@ -433,7 +433,7 @@ function SidebarGroupAction({
         "group-data-[collapsible=icon]:hidden",
         className
       )}
-      {...props}
+      {...props as any}
     />
   )
 }
@@ -519,7 +519,7 @@ function SidebarMenuButton({
       data-size={size}
       data-active={isActive}
       className={cn(sidebarMenuButtonVariants({ variant, size }), className)}
-      {...props}
+      {...props as any}
     />
   )
 
@@ -573,7 +573,7 @@ function SidebarMenuAction({
           "peer-data-[active=true]/menu-button:text-sidebar-accent-foreground group-focus-within/menu-item:opacity-100 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100 md:opacity-0",
         className
       )}
-      {...props}
+      {...props as any}
     />
   )
 }
@@ -694,7 +694,7 @@ function SidebarMenuSubButton({
         "group-data-[collapsible=icon]:hidden",
         className
       )}
-      {...props}
+      {...props as any}
     />
   )
 }
